c64dc1513df8ea4f01c4eef897fd1a4eea0c53d3
AleksandrZakharov58 <91503552+AleksandrZakharov58@users.noreply.github.com>
2022-07-30 16:01:21 +0300

    реализовать роуты /sign-up, /sign-in

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import "../index.css";
 import { useState, useEffect } from "react";
+import { Redirect, Route, Switch } from 'react-router-dom';
 import api from "../utils/Api";
 import Header from "./Header";
 import Main from "./Main";
@@ -10,8 +11,10 @@ import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
 import AddPlacePopup from "./AddPlacePopup";
 import CurrentUserContext from "../contexts/CurrentUserContext";
+import Register from "./Register";
+import Login from "./Login";
 
-function App() {
+export default function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
@@ -19,6 +22,7 @@ function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [cards, setCards] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const isOpen = isEditAvatarPopupOpen || isEditProfilePopupOpen ||
   isAddPlacePopupOpen || selectedCard;
 
@@ -109,53 +113,64 @@ function App() {
 
   return (
     <div className="page">
-      <CurrentUserContext.Provider value={currentUser}>
-        <Header />
-        <Main 
-          onEditAvatar={handleEditAvatarClick}
-          onEditProfile={handleEditProfileClick}
-          onAddPlace={handleAddPlaceClick}
-          onCardClick={handleCardClick}
-          onCardLike={handleCardLike}
-          onCardDelete={handleCardDelete}
-          cards={cards}
-        />
-        <Footer />
-        <PopupWithForm 
-          className="popup popup_type_confirm"
-          name="confirm"
-          title="Вы уверены?"
-          ariaLabel="Да"
-          textButton="Да"
-          onClose={closeAllPopups}
-        >
-        </PopupWithForm>
-        <ImagePopup
-          name={"open-card"}
-          card={selectedCard}
-          onClose={closeAllPopups}
-        />
-        <EditProfilePopup
-          isOpen={isEditProfilePopupOpen}
-          onClose={closeAllPopups}
-          onUpdateUser={handleUpdateUser}
-          isLoading={isLoading}
-        />
-        <EditAvatarPopup 
-          isOpen={isEditAvatarPopupOpen} 
-          onClose={closeAllPopups} 
-          onUpdateAvatar={handleUpdateAvatar}
-          isLoading={isLoading}
-        />
-        <AddPlacePopup
-          isOpen={isAddPlacePopupOpen} 
-          onClose={closeAllPopups} 
-          onAddPlace={handleAddPlaceSubmit}
-          isLoading={isLoading}
-        />
-      </CurrentUserContext.Provider>
+      <Switch>
+        <Route path="/sign-up">
+          <Register />
+        </Route>
+        <Route path="/sign-in">
+          <Login />
+        </Route>
+        <Route>
+          <CurrentUserContext.Provider value={currentUser}>
+            <Header />
+            <Main 
+              onEditAvatar={handleEditAvatarClick}
+              onEditProfile={handleEditProfileClick}
+              onAddPlace={handleAddPlaceClick}
+              onCardClick={handleCardClick}
+              onCardLike={handleCardLike}
+              onCardDelete={handleCardDelete}
+              cards={cards}
+            />
+            <Footer />
+            <PopupWithForm 
+              className="popup popup_type_confirm"
+              name="confirm"
+              title="Вы уверены?"
+              ariaLabel="Да"
+              textButton="Да"
+              onClose={closeAllPopups}
+            >
+            </PopupWithForm>
+            <ImagePopup
+              name={"open-card"}
+              card={selectedCard}
+              onClose={closeAllPopups}
+            />
+            <EditProfilePopup
+              isOpen={isEditProfilePopupOpen}
+              onClose={closeAllPopups}
+              onUpdateUser={handleUpdateUser}
+              isLoading={isLoading}
+            />
+            <EditAvatarPopup 
+              isOpen={isEditAvatarPopupOpen} 
+              onClose={closeAllPopups} 
+              onUpdateAvatar={handleUpdateAvatar}
+              isLoading={isLoading}
+            />
+            <AddPlacePopup
+              isOpen={isAddPlacePopupOpen} 
+              onClose={closeAllPopups} 
+              onAddPlace={handleAddPlaceSubmit}
+              isLoading={isLoading}
+            />
+          </CurrentUserContext.Provider>
+        </Route>
+        <Route>
+          {isLoggedIn ? <Redirect to="/"/> : <Redirect to="/sign-in"/>}
+        </Route>
+      </Switch>
     </div>  
   );
 }
-
-export default App;
