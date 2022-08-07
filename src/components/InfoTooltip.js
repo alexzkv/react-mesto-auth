94d740ada1f64ab10f938f00302f0046c0743278
AleksandrZakharov58 <91503552+AleksandrZakharov58@users.noreply.github.com>
2022-08-07 22:24:59 +0300

    добавить InfoTooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -5,10 +5,12 @@ export default function InfoTooltip({ isOpen, onClose, status }) {
   return (
     <div className={`popup popup_type_infotooltip ${isOpen ? "popup_opened" : ""}`}>
       <div className="popup__container">
-        <img src={status ? successfully : error} alt="Статус регистрации" className="popup__img" />
-        <p className="popup__status">
-          {status ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так!Попробуйте ещё раз.'}
-        </p>
+        <div className="popup__box-infotooltip">
+          <img src={status ? successfully : error} alt="Статус регистрации" className="popup__image" />
+          <p className="popup__status">
+            {status ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так!Попробуйте ещё раз.'}
+          </p>
+        </div>
         <button onClick={onClose} aria-label="Кнопка закрытия" type="button" className="popup__close"></button>
       </div>
     </div>
