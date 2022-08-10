b9bf98509effce8f1a10146f1aa621cf5658f7e4
AleksandrZakharov58 <91503552+AleksandrZakharov58@users.noreply.github.com>
2022-08-10 15:38:28 +0300

    Updates

--- src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Redirect, Route } from 'react-router-dom';
-
-export default function ProtectedRoute({ component: Component, ...props }) {
-  return (
-    <Route>
-      { props.loggedIn ? <Component {...props} /> : <Redirect to='/sign-in' /> }
-    </Route>
-  );
-}
